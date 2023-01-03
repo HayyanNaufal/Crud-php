9d63a696ac20c2be84fedf848034569f898a6ca4
IbuknyaHoamp <sukarnis243@gmail.com>
2023-01-03 03:45:36 -0100

    perubahan file index

--- _Pegawai/index.php
+++ _Pegawai/index.php
@@ -10,34 +10,52 @@
 </head>
 <body>
     <?php require_once('_header.php'); ?>
-    
-    <h1>Pendaftaran Pekerjaan</h1>
-    <form action="_crud.php" method="POST" enctype="multipart/from-data">
-            <label for="nip">NIP :</label>
-            <br>
-            <input type="text"  name="nip">
-            <br>
-            <label for="nama">NAMA :</label>
-            <br>
-            <input type="text" name="nama">
-            <br>
-            <label for="alamat">ALAMAT :</label>
-            <br>
-            <input type="text" name="alamat">
-        <p>Gender : </p>
-            <input type="radio" name="jenis_kelamin" value="Man">
-            <label for="html">Man</label><br>
-            <input type="radio" name="jenis_kelamin" value="Woman">
-            <label for="css">Woman</label><br>
-            <input type="radio" name="jenis_kelamin" value="Alien">
-            <label for="javascript">Alien</label>
-        <br><br>
-        <label for="tanggal_lahir">TANGGAL LAHIR :</label>
-        <br>
-        <input type="date" name="tanggal_lahir">
-        <br><br>
-            <button type="submit" name="input_pekerja" class="btn btn-primary">kirim</button>
-    </form>
+    <div class="container">
+
+        <h1 class="text-center mb-3">Pendaftaran Pekerjaan</h1>
+        <form action="_crud.php" method="POST" enctype="multipart/from-data">
+
+            <div class="mb-3">
+                <label for="nip" class="form-label">NIP :</label>
+                <input type="text"  name="nip" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="nama" class="form-label">NAMA :</label> 
+                <input type="text" name="nama" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="alamat" class="form-label">ALAMAT :</label>
+                <input type="text" name="alamat" class="form-control">
+            </div>
+             
+            <div class="mb-3">
+                <label for="" class="form-label">Gender</label>
+                <div class="row">
+                    <div class="col-md-1">
+                        <input type="radio" name="jenis_kelamin" value="Man">
+                        <label for="html">Man</label><br>
+                    </div>
+                    <div class="col-md-1">
+                        <input type="radio" name="jenis_kelamin" value="Woman">
+                        <label for="css">Woman</label><br>
+                    </div>
+                    <div class="col-md-1">
+                        <input type="radio" name="jenis_kelamin" value="Alien">
+                        <label for="javascript">Alien</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="tanggal_lahir" class="form-label">TANGGAL LAHIR :</label>
+                <input type="date" name="tanggal_lahir" class="form-control">
+            </div>
+            <div class="justify-content-center text-center mt-5">
+                <button type="submit" name="input_pekerja" class="btn btn-primary ">kirim</button>
+            </div>
+        </form>
+    </div>
+
 
     <?php 
         require_once('_footer.php');
